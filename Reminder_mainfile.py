4dd9967fc6952699284cf3f8c717fb0a12ffc341
harsh-k-101 <84068504+harsh-k-101@users.noreply.github.com>
2021-05-24 21:10:31 +0530

    Update Reminder_mainfile.py

--- Reminder_mainfile.py
+++ Reminder_mainfile.py
@@ -4,20 +4,19 @@
 import event_reminder
 import assignment_reminder
 import sedentary_reminder
+import medicine_reminder
 
 from plyer import notification
-
-
 
 
 choice = 0 
 
 print("Hi this is reminder notification program enter your choice:")
-print("For adding a specific reminder press: 1 \nFor setting a sedentry reminder press: 2 \nAssignment Reminder press: 3\nTo exit the app press z \n")
+print("For adding a specific reminder press: 1 \nFor setting a medicine reminder press: 2 \nAssignment Reminder press: 3\nTo exit the app press z \n")
 
 
 while choice != 'z' :
-    choice = input(" Enter your choice: ")
+    choice = input("\nEnter your choice: ")
 
     if choice == '1' :
         event_reminder.event_notification()
@@ -25,14 +24,18 @@ while choice != 'z' :
         #popen('python event_reminder.py ')
 
     elif choice == '2' :
-        sedentary_reminder.run_sedentary()
-        #popen('python sedentary_reminder.py')
+        medicine_reminder.run_medicine()
+        #popen('python medicine_reminder.py')
        
     elif choice == '3' :
         assignment_reminder.run_assignment()
+        #popen('python sedentary_reminder.py')
+
+    elif choice == '4':
+        sedentary_reminder.run_sedentary()
      
     elif choice == 'z':
-        print("Exiting the app: ")
+        print("\nExiting the app: ")
 
     else:
         print("Invalid option !! enter again:")
