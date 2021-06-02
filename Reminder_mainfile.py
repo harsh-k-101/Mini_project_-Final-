a0e05c37a25b6ba806e1847057f2a846104b1b26
harsh-k-101 <84068504+harsh-k-101@users.noreply.github.com>
2021-06-02 13:33:54 +0530

    Update Reminder_mainfile.py

--- Reminder_mainfile.py
+++ Reminder_mainfile.py
@@ -1,6 +1,4 @@
 
-#import subprocess
-
 import event_reminder
 import assignment_reminder
 import sedentary_reminder
@@ -21,15 +19,12 @@ while choice != 'z' :
     if choice == '1' :
         event_reminder.event_notification()
         event_reminder.snooze()
-        #popen('python event_reminder.py ')
 
     elif choice == '2' :
         medicine_reminder.run_medicine()
-        #popen('python medicine_reminder.py')
        
     elif choice == '3' :
         assignment_reminder.run_assignment()
-        #popen('python sedentary_reminder.py')
 
     elif choice == '4':
         sedentary_reminder.run_sedentary()
